6d8727f7d662b30860d70bce21e6b8ece03e4665
Thomas Coding <thomascodinginenglish@gmail.com>
2025-04-10 18:08:37 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,6 @@
 basic.forever(function () {
     basic.showIcon(IconNames.Heart)
-    basic.pause(500)
+    basic.pause(100)
     basic.showIcon(IconNames.SmallHeart)
-    basic.pause(500)
+    basic.pause(100)
 })
