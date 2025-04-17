198274b9ca862099899383940e418375c813d588
Thomas Coding <thomascodinginenglish@gmail.com>
2025-04-17 13:01:34 +0900

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,6 +1,13 @@
 basic.forever(function () {
-    basic.showIcon(IconNames.Heart)
-    basic.pause(1000)
-    basic.showIcon(IconNames.SmallHeart)
-    basic.pause(1000)
+    if (input.lightLevel() < 100) {
+        basic.showLeds(`
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            # # # # #
+            `)
+    } else {
+        basic.clearScreen()
+    }
 })
